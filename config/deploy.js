8c0f29a6136b9e0966120b30e813f5442d589947
Hector Hurtarte <hectorh30@gmail.com>
2018-01-11 18:28:17 -0600

    Update EleccionMP stuff

--- config/deploy.js
+++ config/deploy.js
@@ -22,7 +22,7 @@ module.exports = function(deployTarget) {
 
     // configure other plugins for production deploy target here
     ENV.git = {
-      repo: 'https://github.com/RedCiudadana/MiGuatemala.git',
+      repo: 'https://github.com/RedCiudadana/EleccionMP.git',
       branch: 'gh-pages',
       worktreePath: '/tmp/deploy'
     };
